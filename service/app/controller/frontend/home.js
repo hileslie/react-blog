8aa22f10a212a99883cd5a4badee034dd135ad0c
小蜗 <627054877@qq.com>
2020-09-17 22:59:52 +0800

    feat: getArticleList

--- service/app/controller/frontend/home.js
+++ service/app/controller/frontend/home.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    let res = await this.app.mysql.get('blog_content', [])
-    console.log(res)
-    ctx.body = res;
+    ctx.body = "hi, home";
+  }
+
+  async getArticleList() {
+    const { ctx, app } = this;
+    let sql =
+      "SELECT article.id as id ," +
+      "article.title as title ," +
+      "article.introduce as introduce ," +
+      "article.add_time as add_time ," +
+      "article.view_count as view_count ," +
+      "type.type_name as type_name " +
+      "FROM article LEFT JOIN type ON article.type_id = type.Id";
+
+    const resluts = await app.mysql.query(sql);
+    ctx.body = {
+      data: resluts,
+    };
   }
 }
 
--- service/app/router/frontend.js
+++ service/app/router/frontend.js
@@ -1,4 +1,5 @@
 module.exports = app => {
     const { router, controller } = app;
     router.get('/frontend/index', controller.frontend.home.index);
+    router.get('/frontend/get/article/list', controller.frontend.home.getArticleList);
 }
