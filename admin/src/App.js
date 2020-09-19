de5350cdefad683f9cc4c84d4392d95769ac28e9
小蜗 <627054877@qq.com>
2020-09-19 21:40:21 +0800

    feat: admin

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>xxx</Button>
+    </div>
+  );
+}
+
+export default App;
