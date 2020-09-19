1ea3b47c3325e83ec17ea7ee8c7e31ab4a481025
小蜗 <627054877@qq.com>
2020-09-19 22:33:47 +0800

    feat: layoyt

--- admin/src/pages/login.js
+++ admin/src/pages/login.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {Card, Input, Button, Spin} from 'antd';
+import { UserOutlined , KeyOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import '../static/style/login.scss';
 
 function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
     return (
-        <div>Login</div>
+        <div className="login-div">
+            <Spin tip="loading..." spinning={isLoading}>
+                <Card
+                    title="Leslie"
+                    bordered={true}
+                    style={{width: 400}}
+                >
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="Enter Your UserName"
+                        prefix={<UserOutlined style={{color: 'rgba(0,0,0,.25)'}} />}
+                        onChange={(e) => {setUserName(e.target.value)}}
+                    />
+                    <br /><br />
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="Enter Your UserName"
+                        prefix={<KeyOutlined style={{color: 'rgba(0,0,0,.25)'}} />}
+                        onChange={(e) => {setPassword(e.target.value)}}
+                    />
+                    <br /><br />
+                    <Button type="primary" size="large" block onClick={checkLogin}>登录</Button>
+                </Card>
+            </Spin>
+        </div>
     )
 }
 
--- admin/src/pages/main.js
+++ admin/src/pages/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './login';
+import Layout from './layout';
 
 function Main() {
     return (
         <Router>
             <Route path='/login/' exact component={Login}></Route>
+            <Route path='/index/' exact component={Layout}></Route>
         </Router>
     )
 }
